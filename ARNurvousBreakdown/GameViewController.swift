5031ba1ea8ac8661a3f9a525c1ea8a360755e7c2
佐川晴海[YUMEMI] / Sagawa Harumi <h_sagawa@yumemi.co.jp>
2020-03-29 22:38:38 +0900

    feat: 起動画面を変更しました

---
+++ ARNurvousBreakdown/GameViewController.swift
@@ -0,0 +1,19 @@
+//
+//  GameViewController.swift
+//  ARNurvousBreakdown
+//
+//  Created by 佐川 晴海 on 2020/03/29.
+//  Copyright © 2020 佐川 晴海. All rights reserved.
+//
+
+import UIKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .orange
+        // Do any additional setup after loading the view.
+    }
+
+}
