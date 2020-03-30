259bd1ac7b435e36ba07536c52bacf8a9bd44b5b
佐川晴海[YUMEMI] / Sagawa Harumi <h_sagawa@yumemi.co.jp>
2020-03-30 22:27:53 +0900

    feat: カードの初期表示を行いました

--- ARNurvousBreakdown/GameViewController.swift
+++ ARNurvousBreakdown/GameViewController.swift
@@ -7,13 +7,35 @@
 //
 
 import UIKit
+import RealityKit
 
 class GameViewController: UIViewController {
 
+    @IBOutlet weak var arView: ARView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = .orange
-        // Do any additional setup after loading the view.
+        
+        let anchor:AnchorEntity = AnchorEntity(plane: .horizontal,
+                                               minimumBounds: [0.2, 0.2])
+        arView.scene.addAnchor(anchor)
+        
+        var cards:[Entity]  = []
+        for _ in 1...4 {
+            let box = MeshResource.generateBox(width: 0.04, height: 0.002, depth: 0.04)
+            let metalMaterial = SimpleMaterial(color: .gray, isMetallic: true)
+            let model = ModelEntity(mesh: box, materials: [metalMaterial])
+            model.generateCollisionShapes(recursive: true)
+            cards.append(model)
+            
+        }
+        
+        for (index, card) in cards.enumerated() {
+            let x = Float(index % 2)
+            let z = Float(index / 2)
+            card.position = [x * 0.1, 0, z * 0.1]
+            anchor.addChild(card)
+            
+        }
     }
-
 }
